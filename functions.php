fba96c6f7ce2e11abc394cc973a3a1f96fb75188
Sadat Hossen Himel <sadat.himel@gmail.com>
2021-04-21 14:15:21 +0600

    assets fille add

--- functions.php
+++ functions.php
@@ -1,5 +1,14 @@
-<?php 
+<?php
 
+if (site_url() == "http://demo.lwhh.com") {
+    define("VERSION", time());
+}else{
+    define("VERSION",wp_get_theme()->get("Version"));
+}
+
+/**
+ * after setup theme.
+ */
 
  function philosophy_after_setup_theme(){
  	load_theme_textdomain('philosophy');
@@ -30,6 +39,28 @@
 			)
 		);
  	add_editor_style("/assets/css/editor-style.css");
-
+ }
 
  add_action( 'after_setup_theme', 'philosophy_after_setup_theme');
+
+/**
+ * Enqueue scripts and styles.
+ */
+
+ function philosophy_assets() {
+ 	wp_enqueue_style( 'fontawesome-css', get_theme_file_uri('/assets/css/font-awesome/css/font-awesome.min.css'), null, '1.0' );
+ 	wp_enqueue_style( 'fonts-css', get_theme_file_uri('/assets/css/fonts.css'), null, '1.0' );
+ 	wp_enqueue_style( 'base-css', get_theme_file_uri('/assets/css/base.css'), null, '1.0' );
+ 	wp_enqueue_style( 'vendor-css', get_theme_file_uri('/assets/css/vendor.css'), null, '1.0' );
+ 	wp_enqueue_style( 'main-css', get_theme_file_uri('/assets/css/main.css'), null, '1.0' );
+ 	wp_enqueue_style('philosophy-style-css', get_stylesheet_uri(),null, VERSION);
+
+ 	wp_enqueue_script('modernizr-js', get_theme_file_uri('/assets/js/modernizr.js'), '1.0');
+ 	wp_enqueue_script('pace-min-js', get_theme_file_uri('/assets/js/pace.min.js'), '1.0');
+ 	wp_enqueue_script('plugins-js', get_theme_file_uri('/assets/js/plugins.js'), array('jquery'), '1.0',true);
+ 	wp_enqueue_script('main-js', get_theme_file_uri('/assets/js/main.js'), array('jquery'), '1.0',true);
+ }
+
+add_action('wp_enqueue_scripts', 'philosophy_assets');
+ 
+ 
