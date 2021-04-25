f8fbced5d2dc31dcfc273fb3b5dcaa3f639f0560
Sadat Hossen Himel <sadat.himel@gmail.com>
2021-04-25 12:46:16 +0600

    update post formate

---
+++ template-parts/post-formats/post.php
@@ -0,0 +1,32 @@
+<article class="masonry__brick entry format-standard" data-aos="fade-up">
+
+    <div class="entry__thumb">
+        <a href="single-standard.html" class="entry__thumb-link">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/thumbs/masonry/cookies-400.jpg" 
+                    srcset="<?php echo get_template_directory_uri(); ?>/assets/images/thumbs/masonry/cookies-400.jpg 1x, <?php echo get_template_directory_uri(); ?>/assets/images/thumbs/masonry/cookies-800.jpg 2x" alt="">
+        </a>
+    </div>
+
+    <div class="entry__text">
+        <div class="entry__header">
+            
+            <div class="entry__date">
+                <a href="single-standard.html">December 10, 2017</a>
+            </div>
+            <h1 class="entry__title"><a href="single-standard.html">No Sugar Oatmeal Cookies.</a></h1>
+            
+        </div>
+        <div class="entry__excerpt">
+            <p>
+                Lorem ipsum Sed eiusmod esse aliqua sed incididunt aliqua incididunt mollit id et sit proident dolor nulla sed commodo est ad minim elit reprehenderit nisi officia aute incididunt velit sint in aliqua...
+            </p>
+        </div>
+        <div class="entry__meta">
+            <span class="entry__meta-links">
+                <a href="category.html">Cooking</a>
+                <a href="category.html">Health</a>
+            </span>
+        </div>
+    </div>
+
+</article> <!-- end article -->
