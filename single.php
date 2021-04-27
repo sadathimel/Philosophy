dc88bd102f83a91b122fb2a7b4de24d4ab0f5036
Sadat Hossen Himel <sadat.himel@gmail.com>
2021-04-27 16:25:03 +0600

    update single post

--- single.php
+++ single.php
@@ -47,13 +47,13 @@ get_header();
 
                     <div class="s-content__author-about">
                         <h4 class="s-content__author-name">
-                            <a href="#0">
-                                <?php the_author();?>                                    
+                            <a href="<?php echo esc_url(get_author_posts_url(get_the_author_meta("ID"))); ?>">
+                                <?php the_author(); ?>                                    
                             </a>
                         </h4>
                     
                         <p>
-                            <?php the_author_meta("description");?>
+                            <?php the_author_meta("description"); ?>
                         </p>
 
                         <ul class="s-content__author-social">
@@ -82,16 +82,26 @@ get_header();
                 <div class="s-content__pagenav">
                     <div class="s-content__nav">
                         <div class="s-content__prev">
-                            <a href="#0" rel="prev">
-                                <span>Previous Post</span>
-                                Tips on Minimalist Design 
+                            <?php 
+                            $philosophy_prv_post = get_previous_post();
+                            if($philosophy_prv_post):
+                            ?>
+                            <a href="<?php echo get_the_permalink($philosophy_prv_post); ?>" rel="prev">
+                                <span><?php _e("Previous Post","philosophy"); ?></span>
+                                <?php echo get_the_title($philosophy_prv_post); ?>
                             </a>
+                            <?php endif; ?>
                         </div>
                         <div class="s-content__next">
-                            <a href="#0" rel="next">
-                                <span>Next Post</span>
-                                Less Is More 
+                            <?php 
+                            $philosophy_next_post = get_next_post();
+                            if($philosophy_next_post):
+                            ?>
+                            <a href="<?php echo get_the_permalink($philosophy_next_post);?>" rel="next">
+                                <span><?php _e("Next Post","philosophy"); ?></span>
+                                <?php echo get_the_title($philosophy_next_post); ?>
                             </a>
+                            <?php endif; ?>
                         </div>
                     </div>
                 </div> <!-- end s-content__pagenav -->
