984a10ceb34b8654940994b17f7f3baafd95c2e3
Mahadi001 <sadat.himel@gmail.com>
2021-05-25 14:42:21 +0600

    update

--- functions.php
+++ functions.php
@@ -272,3 +272,17 @@ add_action ( 'category_edit_form_fields', function( $tag ){
         </td>
     </tr> <?php
 });
+
+function philosophy_cpt_slug_fix($post_link,$id){
+    $p = get_post($id);
+    if (is_object($p) && 'chapter' == get_post_type($id)) {
+        $parent_post_id = get_field('parent_book');
+        $parent_post = get_post($parent_post_id);
+        if ($parent_post) {
+            $post_link = str_replace("%book%",$parent_post->post_name,$post_link);
+        }
+        
+    }
+    return $post_link; 
+}
+add_filter('post_type_link', 'philosophy_cpt_slug_fix',1,2 );
