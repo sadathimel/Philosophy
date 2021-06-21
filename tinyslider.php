310380eb22c880a3119a2d7750ccae71d88305ba
Mahadi001 <sadat.himel@gmail.com>
2021-06-21 12:18:56 +0600

    update

---
+++ tinyslider.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *Template Name: TinySlider 
+ */
+?>
+<?php get_header();?>
+
+
+    <!-- s-content
+    ================================================== -->
+    <section class="s-content">
+
+        <div class="row masonry-wrap">
+            <div class="masonry">
+
+            </div> <!-- end masonry -->
+        </div> <!-- end masonry-wrap -->
+
+
+        
+
+    </section> <!-- s-content -->
+
+
+    <?php get_footer();?>
+
